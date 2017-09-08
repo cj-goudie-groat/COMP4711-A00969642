a055d223c632863a0267d0b3145a10d8a51c293f
Connor Goudie <connorgoudie@live.com>
2017-09-07 17:23:54 -0700

    Updated student class and added code to index that displays them

--- PhpProject1/Student.php
+++ PhpProject1/Student.php
@@ -1,16 +1,59 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of Student
- *
+ * Student is a class which has an array of grades, a name, and 
+ * an array of email addresses
  * @author Connor
  */
 class Student {
-    //put your code here
+    /**
+     * Constructor for student
+     */
+    function __construct() {
+        $this->surname = '';
+        $this->first_name = '';
+        $this->emails = array();
+        $this->grades = array();
+    }
+ 
+    /**
+     * Add a new email address to the array
+     * @param type $which
+     * @param type $address
+     */
+    function add_email($which,$address) {
+        $this->emails[$which] = $address;
+    }
+
+    /**
+     * Add a new grade to the array
+     * @param type $grade
+     */
+    function add_grade($grade) {
+        $this->grades[] = $grade;
+    }
+    
+    /**
+     * Calculates student's average grade
+     * @return type
+     */
+    function average() {
+        $total = 0;
+        foreach ($this->grades as $value)
+            $total += $value;
+        return $total / count($this->grades);
+    }
+    
+    /**
+     * Prints out the student's data in a formatted manner
+     * @return type
+     */
+    function toString() {
+        $result = $this->first_name . ' ' . $this->surname;
+        $result .= ' ('.$this->average().")\n";
+        foreach($this->emails as $which=>$what)
+            $result .= $which . ': '. $what. "\n";
+        $result .= "\n";
+        return '<pre>'.$result.'</pre>';
+    }
 }
--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -11,7 +11,52 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            echo 'we did it reddit?';
+            include('Student.php');
+
+            $students = array();
+            
+            $first = new Student();
+            $second = new Student();
+            $third = new Student();
+
+            // Creates a good student named John Doe
+            $first->surname = "Doe";
+            $first->first_name = "John";
+            $first->add_email('home','john@doe.com');
+            $first->add_email('work','jdoe@mcdonalds.com');
+            $first->add_grade(65);
+            $first->add_grade(75);
+            $first->add_grade(55);
+            $students['j123'] = $first;
+
+            // Creates a great student named Albert Einstein
+            $second->surname = "Einstein";
+            $second->first_name = "Albert";
+            $second->add_email('home','albert@braniacs.com');
+            $second->add_email('work1','a_einstein@bcit.ca');
+            $second->add_email('work2','albert@physics.mit.edu');
+            $second->add_grade(95);
+            $second->add_grade(80);
+            $second->add_grade(50);
+            $students['a456'] = $second;
+            
+            // Creates a new evolution of humanity
+            // in student form named Connor Goudie
+            $third->surname = "Connor";
+            $third->first_name = "Goudie";
+            $third->add_email('home','connorgoudie@live.com');
+            $third->add_grade(100);
+            $third->add_grade(99);
+            $third->add_grade(100.0000000001);
+            $students['c789'] = $third;
+
+            ksort($students);
+            
+            //loops through the student array and prints each one
+            foreach($students as $student) {
+                echo $student->toString();
+                echo "\n\n";
+            }
         ?>
     </body>
 </html>
